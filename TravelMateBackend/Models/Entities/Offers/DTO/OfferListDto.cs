b71bec885536f597bf58833fcb4664f6f1b79c98
Tomasz Krezymon <61662701+komeg1@users.noreply.github.com>
2025-04-26 18:52:47 +0200

    Feature/rswd 11 implement basic backend (#2)
    * add basic structure of repository,service,controller for offers microservice
    
    * fix configuration to use mysql instead of mssql
    
    * make airport name nullable
    
    * fix wrong http redirect for swagger
    
    * add offerlistdto, add mapper for the dto
    
    * delete user booking payment from offer service

---
+++ TravelMateBackend/Models/Entities/Offers/DTO/OfferListDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelMateBackend.Models.Entities.Offers.DTO
+{
+    public class OfferListDto
+    {
+        public Guid Id { get; set; }
+        public string AirlineName { get; set; }
+        public string FlightNumber { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public decimal BasePrice { get; set; }
+    }
+}
---
+++ TravelMateBackend/Services/Offers/IOfferService.cs
@@ -0,0 +1,14 @@
+﻿using Models.Entities.Offers;
+using TravelMateBackend.Models.Entities.Offers.DTO;
+
+namespace TravelMateBackend.Services.Offers
+{
+    public interface IOfferService
+    {
+        Task<IEnumerable<OfferListDto>> GetOffers();
+        Task<Offer> GetOffer(Guid id);
+        Task<Offer> AddOffer(Offer offer);
+        Task<Offer> UpdateOffer(Offer offer);
+        Task<Offer> DeleteOffer(int id);
+    }
+}
