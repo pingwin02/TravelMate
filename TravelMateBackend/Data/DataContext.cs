5980ae0ccbeb169f7bc50ebd267882abf15e6ec8
komeg1 <tomekkrezymon@gmail.com>
2025-04-26 18:29:42 +0200

    delete user booking payment from offer service

--- TravelMateBackend/Data/DataContext.cs
+++ TravelMateBackend/Data/DataContext.cs
@@ -10,23 +10,11 @@ namespace TravelMateBackend.Data
         {
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Airline> Airlines { get; set; }    
         public DbSet<Airplane> Airplanes { get; set; }
         public DbSet<Airport> Airports { get; set; }
-        public DbSet<Booking> Bookings { get; set; }
         public DbSet<Offer> Offers { get; set; }
-        public DbSet<Payment> Payments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Payment>()
-                .HasOne(p => p.Booking)
-                .WithOne(b => b.Payment)
-                .HasForeignKey<Payment>(p => p.BookingId)
-                .IsRequired(); 
-        }
     }
 }
