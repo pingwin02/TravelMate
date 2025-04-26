386be7501aad677ff1265cbe8f7c6f3dd9551010
Tomasz Krezymon <61662701+komeg1@users.noreply.github.com>
2025-04-26 18:44:47 +0200

    Feature/rswd 6 scraper for transportation data (#1)
    * add initial destinations scrapper
    
    * add airports, flights scrapping
    
    * add try catches for edge cases
    
    * add requirements.txt
    
    * add price extraction, map zoom out, initial script for sql commits
    
    * add offers generator
    
    * fixes to sql injector script
    
    * refactor flight data processing and update SQL queries for offers and planes
    
    * add seat count randomizer for plane record

---
+++ scraper/const.py
@@ -0,0 +1,14 @@
+FROM = "ROM"
+FROM_FULL = "Rome"
+FROM_COUNTRY = "Italy"
+TO = "anywhere"
+STOPS = 0
+
+START_URL = f"https://www.kayak.com/explore/{FROM}-{TO}?stops={STOPS}"
+
+
+DESTINATION_PATH = "results/destinations.json"
+FLIGHTS_PATH = "results/flights.json"
+AIRPORTS_PATH = "results/airports.json"
+AIRLINES_PATH = "results/airlines.json"
+PLANES_PATH = "results/planes.json"
