9aa03b8032a0f92c99420fafdba60126a7a797bf
Tomasz Krezymon <61662701+komeg1@users.noreply.github.com>
2025-04-26 19:19:39 +0200

    Feature/rswd 13 implement user microservice (#3)
    * add login endpoint that returns jwt token if credentials are correct
    
    * add guid to jwt token

---
+++ TravelMateAuthService/TravelMateAuthService/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelMateAuthService.Entities;
+
+namespace TravelMateAuthService.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+           : base(options) { }
+
+        public DbSet<UserCredentials> Users { get; set; }
+    }
+}
