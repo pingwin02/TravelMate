719702e70c928ada2f3a61687ca7e50ec8fde851
kalinovsk1 <robert.kali0204@gmail.com>
2025-05-07 03:05:17 +0200

    add parameter to run command for setting the number of offers to be booked

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,27 @@
 from selenium_scripts import init, login_user, book_offers, book_offer_timeout
+import sys
+
+def user_input():
+    if len(sys.argv) > 1:
+        try:
+            num_of_offers = int(sys.argv[1])
+            return num_of_offers
+        except ValueError:
+            print("Given argument is not an integer")
+            return None
+    else:
+        print("No argument given")
+        return None
+
 
 if __name__ == "__main__":
+    num_of_offers = user_input()
 
-    driver = init()
+    if num_of_offers is not None:
+        driver = init()
 
-    login_user(driver)
-    book_offers(driver, 5)
-    book_offer_timeout(driver)
+        login_user(driver)
+        book_offers(driver, num_of_offers)
+        book_offer_timeout(driver)
 
-    driver.quit()
+        driver.quit()
