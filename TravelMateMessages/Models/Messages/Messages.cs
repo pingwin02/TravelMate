1547e6352bceb1c277fc7297bbdf4a7604a06119
komeg1 <tomekkrezymon@gmail.com>
2025-06-06 09:48:32 +0200

    add backend part of offer purchase notification system

--- TravelMateMessages/Models/Messages/Messages.cs
+++ TravelMateMessages/Models/Messages/Messages.cs
@@ -133,7 +133,11 @@ public class DeleteOfferEvent
 {
     public Guid OfferId { get; set; }
 }
-
+public class PurchaseNotificationEvent
+{
+    public Guid CorrelationId { get; set; }
+    public Guid OfferId { get; set; }
+}
 public enum BookingStatus
 {
     Pending,
--- TravelMateOfferQueryService/Hubs/OfferHub.cs
+++ TravelMateOfferQueryService/Hubs/OfferHub.cs
@@ -4,4 +4,14 @@ namespace TravelMateOfferQueryService.Hubs;
 
 public class OfferHub : Hub
 {
+    public async Task JoinOfferGroup(string offerId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, offerId);
+        await Clients.Group(offerId).SendAsync("UserJoined", $"User {Context.ConnectionId} joined offer {offerId}");
+    }   
+    public async Task LeaveOfferGroup(string offerId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, offerId);
+        await Clients.Group(offerId).SendAsync("UserLeft", $"User {Context.ConnectionId} left offer {offerId}");
+    }
 }
